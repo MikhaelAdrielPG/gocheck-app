117d37013f329479051a877c0832ecb3a1e35724
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-08-03 09:11:10 +0700

    Contoh Implementasi Deriving State

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToogleItem}
       />
-      <Stats />
+      <Stats items={listItems} />
     </div>
   );
 }
@@ -111,10 +111,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (items.length === 0) {
+    return (
+      <footer className="stats">
+        <span>🗒️ Yuk mulai bikin catatan!</span>
+      </footer>
+    );
+  }
+
+  const totalItems = items.length;
+  const doneItems = items.filter((item) => item.done).length;
+  const percentage = Math.round((doneItems / totalItems) * 100);
   return (
     <footer className="stats">
-      <span>🗒️ Kamu punya x catatan dan baru x yg dichecklist (x%) ✅</span>
+      <span>
+        {percentage === 100
+          ? "✅ Kamu sudah melakukannya semua"
+          : `🗒️ Kamu punya ${totalItems} catatan dan baru ${doneItems} dichecklist yang (
+        ${percentage}%) `}
+      </span>
     </footer>
   );
 }
